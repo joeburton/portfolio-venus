3f285212fe00347aaed86d4a03d52e8c37b093e6
Joe Burton <joeburton@gmail.com>
2024-07-10 16:08:17 +0100

    chore: disabling projects api

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,31 +1,7 @@
 import { NextResponse } from "next/server";
 
-// import { projects } from "./projects";
+import { projects } from "./projects";
 
 export async function GET() {
-  return NextResponse.json(
-    [
-      {
-        logo: "guardian.png",
-        logoSize: "small",
-        className: "guardian-logo",
-        role: "Front-end Developer",
-        company: "Guardian",
-        description:
-          "<p>I worked for the Guardian as a Front-end Developer creating HTML/ CSS cross browser/ platform compliant templates for www.thegreatplanthunt.org.</p>",
-        skills: "JavaScript, jQuery, HTML, CSS3",
-        links: [
-          {
-            visual: "www.theguardian.com",
-            url: "https://www.theguardian.com/",
-          },
-          {
-            visual: "www.greatplanthunt.org",
-            url: "http://www.greatplanthunt.org",
-          },
-        ],
-      },
-    ],
-    { status: 200 }
-  );
+  return NextResponse.json(projects, { status: 200 });
 }
