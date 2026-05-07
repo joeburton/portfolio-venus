ffe12c2fb038892b0108630dae073c2b7a01f472
Joe Burton <joeburton@gmail.com>
2026-05-07 14:05:45 +0100

    chore: contact update

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -7,19 +7,19 @@ export default async function Thankyou() {
   return (
     <>
       <PageIntro
-        pageTitle='Thank you'
-        subText={<>I&apos;ll get back to you as soon as I can.</>}
+        pageTitle="Thank you"
+        subText={<>I&apos;ll be intouch as soon as I can.</>}
       />
       <div className={styles.thankyou}>
-        <Box maxWidth='420px' m='0 auto'>
-          <Card variant='elevated'>
+        <Box maxWidth="420px" m="0 auto">
+          <Card variant="elevated">
             <CardBody>
               <SimpleGrid columns={1}>
                 <Box>
                   <Show>
                     <Image
-                      src='/assets/pigeons-ai/carrier-pigeon-ai-10.png'
-                      alt='Cliché image of a carrier pigeon. This image was generated using AI 🤓 how ironic.'
+                      src="/assets/pigeons-ai/carrier-pigeon-ai-10.png"
+                      alt="Cliché image of a carrier pigeon. This image was generated using AI 🤓 how ironic."
                     />
                   </Show>
                 </Box>
